5a047d241f3f5c48164e95dd870af140b63f01a3
Ilay Gilat <gilat.ilay@gmail.com>
2022-09-17 18:46:58 +0300

    Styles and Fixes

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,32 @@
 import { useRouter } from "next/router";
 import { useState } from "react";
+import Head from "next/head";
 export default function Home() {
   const [roomID, setRoomID] = useState("");
   const router = useRouter();
   return (
-    <div>
-      <input
-        type="text"
-        value={roomID}
-        onChange={(e) => setRoomID(e.target.value)}
-        className="bg-transparent border"
-      />
-      <button onClick={() => router.push(`/room/${roomID}`)}>Submit</button>
+    <div className="flex  h-screen w-screen flex-col justify-between items-center relative">
+      <Head>
+        <title>Sten Video Chat</title>
+      </Head>
+      <div className="text-2xl text-center pt-8">Sten Video Chat</div>
+      <div className=" flex my-auto justify-center items-center">
+        <input
+          type="text"
+          value={roomID}
+          onChange={(e) => setRoomID(e.target.value)}
+          className="bg-transparent outline outline-1 outline-offset-4"
+        />
+        <div className="p-4" />
+        <button
+          onClick={() => {
+            roomID ? router.push(`/room/${roomID}`) : alert("Need Room ID");
+          }}
+          className="outline outline-1 rounded outline-offset-4  cursor-pointer"
+        >
+          Submit
+        </button>
+      </div>
     </div>
   );
 }
--- pages/room/[id].js
+++ pages/room/[id].js
@@ -1,9 +1,11 @@
 import React from "react";
 import { useRouter } from "next/router";
+import Head from "next/head";
 import { useEffect, useState } from "react";
-const Profile = () => {
+const VideoChat = () => {
   const { query } = useRouter();
   const [id, setId] = useState();
+  const [message, SetMessage] = useState("");
   let localStream;
   useEffect(() => {
     setId(query.id);
@@ -19,34 +21,53 @@ const Profile = () => {
     });
     console.log(localStream);
     document.getElementById("user-1").srcObject = localStream;
+    document.getElementById("user-2").srcObject = localStream;
   };
   return id ? (
-    <div>
-      <div>ROOM - {id}</div>
-      <div className="grid grid-cols-2 gap-8">
+    <div className="flex flex-col justify-between items-center relative">
+      <Head>
+        <title>Room {id}</title>
+      </Head>
+      <div className="text-2xl text-center pt-8">ROOM - {id}</div>
+      <div className="p-8 flex justify-between items-center flex-col md:flex-row animate-fade-in">
         <video
           autoPlay
-          playsInline
-          className="bg-black h-[300px]"
+          className="bg-black h-[300px] outline outline-offset-8 outline-1"
           id="user-1"
         ></video>
         <div className="p-4" />
         <video
           id="user-2"
-          className="bg-black h-[300px]"
+          className="bg-black h-[300px] outline outline-offset-8 outline-1"
           autoPlay
-          playsInline
         ></video>
       </div>
       <div className="p-4" />
-      <div>
-        <textarea className="bg-transparent border" />
-        <button>Submit</button>
+      <div className=" flex my-auto justify-center items-center">
+        <input
+          type="text"
+          value={message}
+          onChange={(e) => SetMessage(e.target.value)}
+          className="bg-transparent outline outline-1 outline-offset-4"
+        />
+        <div className="p-4" />
+        <button
+          onClick={() => {
+            roomID ? alert(message) : alert("Need Room ID");
+          }}
+          className="outline outline-1 rounded outline-offset-4  cursor-pointer"
+        >
+          Submit
+        </button>
       </div>
+      <div className="p-8" />
+      <h1 className="text-center font-bold text-4xl">
+        Last Message: {message ? message : "No Message"}
+      </h1>
     </div>
   ) : (
     <div>loading</div>
   );
 };
 
-export default Profile;
+export default VideoChat;
